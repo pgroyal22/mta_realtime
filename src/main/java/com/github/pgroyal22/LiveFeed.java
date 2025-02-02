4a72c2f8062066f2d7b70f74dd0ee8942d5f829b
Phil Genovese <tuj57093@temple.edu>
2025-02-02 16:12:40 -0500

    combined NYCT extensions with general feed

--- src/main/java/com/github/pgroyal22/LiveFeed.java
+++ src/main/java/com/github/pgroyal22/LiveFeed.java
@@ -1,19 +1,23 @@
 package com.github.pgroyal22;
 
+import com.google.protobuf.ExtensionRegistry;
 import com.google.transit.realtime.GtfsRealtime;
 import com.google.transit.realtime.GtfsRealtimeNYCT;
-import jakarta.inject.Singleton;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 public class LiveFeed {
 
-    private GtfsRealtime.FeedMessage feed;
+    private final GtfsRealtime.FeedMessage feed;
 
     public LiveFeed(URL url) throws IOException {
-        feed = GtfsRealtime.FeedMessage.parseFrom(url.openStream());
+
+        ExtensionRegistry registry = ExtensionRegistry.newInstance();
+        registry.add(GtfsRealtimeNYCT.nyctFeedHeader);
+        registry.add(GtfsRealtimeNYCT.nyctStopTimeUpdate);
+        registry.add(GtfsRealtimeNYCT.nyctTripDescriptor);
+        feed = GtfsRealtime.FeedMessage.parseFrom(url.openStream(), registry);
     }
 
     public GtfsRealtime.FeedMessage getFeed() {
